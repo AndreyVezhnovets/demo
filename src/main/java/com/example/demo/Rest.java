03dc72be41b88d8e07a42f97068a08d98b9ddba4
andrey <avezhnavets@exadel.com>
2019-01-26 15:35:43 +0300

    Initial commit

---
+++ src/main/java/com/example/demo/Rest.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Rest {
+
+    @RequestMapping("/get")
+    public String get() {
+        return "шалом";
+    }
+}
